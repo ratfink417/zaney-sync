e418454232d0981600d54a7e54878a490cfabb0a
ratfink417 <ratfink417@gmail.com>
2024-03-24 19:24:49 -0700

    open in tmux by default

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,5 +1,17 @@
 { config, pkgs, lib, username, ... }:
-
+let
+  tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
+    {
+      pluginName = "tmux-super-fingers";
+      version = "unstable-2023-01-06";
+      src = pkgs.fetchFromGitHub {
+        owner = "artemave";
+        repo = "tmux_super_fingers";
+        rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
+        sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
+      };
+    };
+in
 {
   # install packages
   home.packages = with pkgs; [
@@ -16,9 +28,14 @@
     newSession = true; # Automatically spawn a session if trying to attach and none are running. (this should make it okay to ditch initerm)
     prefix = "C-a";
 
+    # install tmux plugins
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
       tmux-fzf
+      {
+          plugin = tmux-super-fingers;
+          extraConfig = "set -g @super-fingers-key f";
+        }
     ];
   extraConfig = ''
     # Smart pane switching with awareness of Vim splits.
